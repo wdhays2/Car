75032a497b41567530e661c89bbe6f42c057051f
wdhays2 <wdhays2@charter.net>
2015-02-04 15:37:15 -0800

    Fixed some formatting.

--- carAssignment.js
+++ carAssignment.js
@@ -3,81 +3,74 @@ var tire {
 
 	brand: "B.F. Goodrich", model: "Comp T/A", currentPressure: 22,
 
-		checkPressure: function (){
+	checkPressure: function (){
 		return currentPressure
-		}
+	}
 
-		adjustPressure: function (addOrRemoveAir){
-		currentPressure += addOrRemoveAir;
-		return currentPressure;
-
-		if (currentPressure < minPressure) {alert("Tire pressure is low. 
-		It currently has " + currentPressure + " psi.")
+	adjustPressure: function (addOrRemoveAir){
+		if ((currentPressure + addOrRemoveAir) < minPressure) {
+			alert("That would give you " + (currentPressure + addOrRemoveAir) + "psi.");
 		} 
-		else if (currentPressure > maxPressure) {alert("Tire pressure is 
-		too high. It currently has " + currentPressure + " psi.")
+		else if ((currentPressure + addOrRemoveAir) > maxPressure) {
+			alert("That would give you " + (currentPressure + addOrRemoveAir) + "psi.");
 		} 
-		else {alert("The tire is properly inflated to " + 
-		currentPressure + " psi.")
+		else {
+			currentPressure += addOrRemoveAir;
+			alert("The tire is properly inflated to " + currentPressure + "psi.");
 		}
-		}
+	}
 };
-var leftFront = Object.create(tire);
-
-var rightFront = Object.create(tire);
-
-var leftRear = Object.create(tire);
-
-var rightRear = Object.create(tire);
 
 var car { 
 	make: "Pontiac", model: "Trans Am", year: 1978, 
 	sizeGasTank: 20, maxSpeed: 135, minSpeed: 0, cityMPG: 15,
 	hwyMPG: 20, speedingMPG: 17, racingMPG: 13, gasInTank: 7,
 	currentSpeed: 0,
-	wheels: {leftFront, rightFront, leftRear, rightRear},
+	wheels: [leftFront: Object.create(tire), rightFront: Object.create(tire), 
+	leftRear: Object.create(tire), rightRear: Object.create(tire)],
 
-		checkGas: function (){
+	checkGas: function (){
 		return gasInTank
-		}
+	}
 
-		addGas: function (gallons){
-		gasInTank += gallons
-		return gasInTank;
+	addGas: function (gallons){
+		
 
-		if (gasInTank > sizeGasTank) {alert("The tank only holds " + 
-		sizeGasTank + " gallons.\n" 
-		"Use a negative number to remove some gas.")
+		if ((gasInTank + gallons) > sizeGasTank) {
+			alert("The tank only holds " + sizeGasTank + " gallons.");
 		} 
-		else if (gasInTank <= .25*sizeGasTank){ alert("The tank is 
-		almost empty.")
+		else if ((gasInTank + gallons) <= .25*sizeGasTank){ 
+			alert("The tank is almost empty.");
 		}
-		else if (gasInTank >= .95*sizeGasTank){alert("The tank is full.")
+		else if {
+		gasInTank += gallons;
+		(gasInTank >= .95*sizeGasTank){
+			alert("The tank is full.");
 		}
-		else {alert("Did you want to fill it up?")
+		else {
+			alert("Did you want to fill it up? There are " + gasInTank + " gallons in the tank.");
 		}
-		},
+	},
 
-		speedUp: function (speed){
-		currentSpeed += speed; 
-		return currentSpeed;
-
-		if (currentSpeed > maxSpeed){alert("Damnit Jim, I'm giving it
-			all I've got.\n" 
-			"I can't seem to break " + maxSpeed + "mph.\n"
-			"You're trying to push it to " + currentSpeed + "mph."
-			"You're going to have to use the slowDown function.")
-		} else {alert("Current speed is " + currentSpeed + ".")
+	speedUp: function (speed){
+		if ((currentSpeed + speed) > maxSpeed){
+			alert("Damnit Jim, I'm giving it all I've got.\n" 
+			"That would be " + (currentSpeed + speed) + "mph.\n");
+		} 
+		else {
+			currentSpeed += speed;
+			return currentSpeed;
 		}
+	},
+	slowDown: function (speed){
+		if ((currentSpeed - speed) < minSpeed){
+			alert("I can't go " + (currentSpeed - speed) + "mph.");
 		}
-
-		slowDown: function (speed){
-		currentSpeed -= speed; 
-		return currentSpeed;
-		if (currentSpeed < minSpeed){alert("We cant go back in time.\n"
-		"Use the speedUp function to get a ")}
-
+		else{
+			currentSpeed -= speed; 
+			return currentSpeed;
 		}
+	},
 
 
 }
